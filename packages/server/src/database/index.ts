d55e12d55e6162f5d89b7e46be62aeaa735ec895
thauandev <thauandepaula@icloud.com>
2020-08-21 16:25:54 -0300

    Repositório do TypeORM

--- packages/server/src/database/index.ts
+++ packages/server/src/database/index.ts
@@ -1,3 +1,13 @@
 import { createConnection } from 'typeorm'
 
-createConnection()
+createConnection({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'gostack_gobarber',
+  entities: ['../models/*.ts'],
+  synchronize: true,
+  logging: false
+})
